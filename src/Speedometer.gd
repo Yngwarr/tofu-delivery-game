d0e712aa2034d3e7b5ffeb055b5eccf123e103b7
Yngvarr <k.yngvarr@gmail.com>
2023-04-30 19:20:52 +0200

    wired the speedometer

--- src/Speedometer.gd
+++ src/Speedometer.gd
@@ -1,8 +1,12 @@
-extends Label
+extends TextureRect
 
 @export var car: RigidBody3D
+
+@onready var label: Label = $Text
+@onready var arrow: Sprite2D = $Arrow
 
 func _physics_process(_delta):
 	# converting to km/h
 	var speed = car.get_linear_velocity().length() * 3.6
-	text = "%.f" % speed
+	label.text = "%.f" % speed
+	arrow.rotation_degrees = 270.0 * (speed / 150)
